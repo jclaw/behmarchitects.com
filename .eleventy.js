e4b0ca410fe8b8844f27668f01feea0b336f4a64
Jackson Clawson <jclaw3456@gmail.com>
2024-09-15 15:55:59 -0400

    add site files

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('img')
+  eleventyConfig.addPassthroughCopy('css')
+  eleventyConfig.addPassthroughCopy('js')
 
   eleventyConfig.addWatchTarget('css/')
   eleventyConfig.addWatchTarget('js/')
